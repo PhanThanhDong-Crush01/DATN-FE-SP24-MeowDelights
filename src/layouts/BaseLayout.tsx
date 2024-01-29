1115ef13eb9e6e89b47ab451d4d69decb036e2f7
PhanThanhDong_Crush <dongptph28020@fpt.edu.vn>
2024-01-30 02:44:46 +0700

    feat: create ui client home + menu + footer ref: #17

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,16 +1,23 @@
 import FooterClientComponent from '@/components/component/FooterClientComponent'
 import HeaderClientComponent from '@/components/component/HeaderClientComponent'
 import { Outlet } from 'react-router-dom'
+import '../assets/css/plugins/bootstrap.min.css'
+import '../assets/css/plugins/slick.css'
+import '../assets/css/plugins/ion.rangeSlider.min.css'
+import '../assets/css/plugins/magnific-popup.css'
+import '../assets/css/style.css'
+import '../assets/fonts/flaticon/flaticon.css'
+import '../assets/fonts/font-awesome.min.css'
 
 const BaseLayout = () => {
     return (
-        <div className='max-w-7xl mx-auto'>
+        <div style={{ width: '100%' }}>
             <HeaderClientComponent />
-
             <main>
                 <Outlet />
             </main>
             <FooterClientComponent />
+            <script src='../assets/js/main.js'></script>
         </div>
     )
 }

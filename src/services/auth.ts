9eb02eaab307fd43e106576a7bef46e4c18b5ecc
PhanThanhDong_Crush <dongptph28020@fpt.edu.vn>
2024-02-03 06:08:25 +0700

    feat: [FE] new new cart + bill + add product

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -3,46 +3,17 @@
 
 import { IAuth } from '@/interface/IAuth'
 import instance from './core/api'
-export const getAll = async () => {
-    try {
-        const response = await instance.get('/auth')
-        return response.data
-    } catch (error) {
-        console.log(`['GETALL_AUTH_ERROR']`, error)
-    }
-}
-export const getOne = async (id: string) => {
-    try {
-        const response = await instance.get(`/auth/${id}`)
-        return response.data
-    } catch (error) {
-        console.log(`['GETONE_AUTH_ERROR']`, error)
-    }
-}
 
-export const signin = async (user: any) => {
-    try {
-        const response = await instance.post('/auth/signin', user)
-        return response.data
-    } catch (error) {
-        console.log(`['Signin_ERROR']`, error)
-    }
-}
-export const signup = async (user: any) => {
-    try {
-        const response = await instance.post('/auth/signup', user)
-        return response.data
-    } catch (error) {
-        console.log(`['GETONE_CART_ERROR']`, error)
-    }
-}
+// export const getAll = async () => {}
+// export const getOne = async (id: string) => {}
+// export const update = async (user: IAuth) => {}
+// export const add = async (user: IAuth) => {}
+// export const remove = async (user: IAuth) => {}
+// //mẫu product
 
-export const updateUserProfile = async (user: IAuth) => {
-    try {
-        console.log(user)
-        const response = await instance.patch(`/auth/${user._id}`, user)
-        return response.data
-    } catch (error) {
-        console.log(`['UPDATEUSERPROFILE_AUTH_ERROR']`, error)
-    }
+export const signin = (user: any) => {
+    return instance.post('/auth/signin', user)
+}
+export const signup = (user: any) => {
+    return instance.post('/auth/signup', user)
 }

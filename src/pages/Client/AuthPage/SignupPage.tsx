48ccee69c631bafa0ff318f105a6a8123ad4023b
hoa phung <hoaptph29645@fpt.edu.vn>
2024-02-15 01:01:19 +0700

    feat: cắm API updateProfileUser ref: #42

--- src/pages/Client/AuthPage/SignupPage.tsx
+++ src/pages/Client/AuthPage/SignupPage.tsx
@@ -3,6 +3,8 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import { message } from 'antd'
 import { signup } from '@/services/auth'
+import MenuClientComponent from '@/components/component/MenuClientComponent'
+import FooterTemplate from '@/components/component/Footer'
 
 const SignupPage = () => {
     const navigate = useNavigate()
@@ -15,6 +17,7 @@ const SignupPage = () => {
             const reponse = await signup(data)
             console.log('api signup', reponse)
             message.success(reponse?.data?.message)
+            // localStorage.setItem('user', JSON.stringify(data))
             navigate('/signin')
         } catch (error: any) {
             console.log(error)
@@ -23,111 +26,140 @@ const SignupPage = () => {
     }
 
     return (
-        <div>
-            <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl'>
-                <div className='hidden bg-cover lg:block lg:w-1/2 pt-4 pl-4'>
-                    <img src='https://i.pinimg.com/564x/f1/85/01/f18501bb5948f2789c8db87d1f376e42.jpg' alt='' />
+        <>
+            <div className=' '>
+                <MenuClientComponent />
+
+                <div className='sigma_subheader style-5 bg-gray '>
+                    <div className='container'>
+                        <div className='sigma_subheader-inner'>
+                            <h1>Trang sản phẩm</h1>
+                        </div>
+                        <ol className='breadcrumb'>
+                            <li className='breadcrumb-item'>
+                                <a className='btn-link' href='#'>
+                                    Trang chủ
+                                </a>
+                            </li>
+                            <li className='breadcrumb-item active' aria-current='page'>
+                                Trang đăng ký
+                            </li>
+                        </ol>
+                    </div>
+
+                    <img src='src/assets/img/subheader-br.png' className='br' alt='subheader' />
+                    <img src='src/assets/img/subheader-bl.png' className='bl' alt='subheader' />
+                    <img src='src/assets/img/subheader-tr.png' className='tr' alt='subheader' />
                 </div>
-                <div className='w-full px-6 py-8 md:px-8 lg:w-1/2'>
-                    <div className='flex justify-center mx-auto'>
-                        <img className='w-auto h-7 sm:h-8' src='https://merakiui.com/images/logo.svg' alt='' />
+                <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl  my-5 '>
+                    <div className='hidden bg-cover lg:block lg:w-1/2 pt-4 pl-4'>
+                        <img src='https://i.pinimg.com/564x/f1/85/01/f18501bb5948f2789c8db87d1f376e42.jpg' alt='' />
                     </div>
-                    <p className='mt-3 text-xl text-center text-gray-600 dark:text-gray-200'>Đăng ký!</p>
+                    <div className='w-full px-6 py-8 md:px-8 lg:w-1/2'>
+                        <div className='flex justify-center mx-auto'>
+                            <img className='w-auto h-7 sm:h-8' src='https://merakiui.com/images/logo.svg' alt='' />
+                        </div>
+                        <p className='mt-3 text-xl text-center text-gray-600 dark:text-gray-200'>Đăng ký!</p>
 
-                    <div className='flex items-center justify-between mt-4'>
-                        <span className='w-1/5 border-b dark:border-gray-600 lg:w-1/4'></span>
-                        <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
-                    </div>
-
-                    <form onSubmit={handleSubmit(onSubmit)} className='mt-4'>
-                        <div className='mb-4'>
-                            <label
-                                htmlFor='LoggingName'
-                                className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
-                            >
-                                Họ Tên
-                            </label>
-                            <input
-                                id='LoggingName'
-                                {...register('name')}
-                                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
-                                type='text'
-                            />
+                        <div className='flex items-center justify-between mt-4'>
+                            <span className='w-1/5 border-b dark:border-gray-600 lg:w-1/4'></span>
+                            <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
                         </div>
 
-                        <div className='mb-4'>
-                            <label
-                                htmlFor='LoggingEmailAddress'
-                                className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
-                            >
-                                Email
-                            </label>
-                            <input
-                                {...register('email')}
-                                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
-                                type='email'
-                            />
-                        </div>
-                        <div className='mb-4'>
-                            <label
-                                htmlFor='loggingPassword'
-                                className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
-                            >
-                                Mật khẩu
-                            </label>
-                            <input
-                                id='loggingPassword'
-                                {...register('password')}
-                                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
-                                type='password'
-                            />
-                        </div>
+                        <form onSubmit={handleSubmit(onSubmit)} className='mt-4'>
+                            <div className='mb-4'>
+                                <label
+                                    htmlFor='LoggingName'
+                                    className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
+                                >
+                                    Họ Tên
+                                </label>
+                                <input
+                                    id='LoggingName'
+                                    {...register('name')}
+                                    className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
+                                    type='text'
+                                />
+                            </div>
 
-                        <div className='mb-4'>
-                            <label
-                                htmlFor='confirmPassword'
-                                className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
-                            >
-                                Xác nhận mật khẩu
-                            </label>
-                            <input
-                                {...register('confirmPassword')}
-                                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
-                                type='password'
-                            />
-                        </div>
+                            <div className='mb-4'>
+                                <label
+                                    htmlFor='LoggingEmailAddress'
+                                    className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
+                                >
+                                    Email
+                                </label>
+                                <input
+                                    {...register('email')}
+                                    className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
+                                    type='email'
+                                />
+                            </div>
+                            <div className='mb-4'>
+                                <label
+                                    htmlFor='loggingPassword'
+                                    className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
+                                >
+                                    Mật khẩu
+                                </label>
+                                <input
+                                    id='loggingPassword'
+                                    {...register('password')}
+                                    className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
+                                    type='password'
+                                />
+                            </div>
 
-                        <div className='mt-6'>
-                            <button
-                                type='submit'
-                                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
-                            >
-                                Đăng ký
-                            </button>
-                        </div>
-                    </form>
+                            <div className='mb-4'>
+                                <label
+                                    htmlFor='confirmPassword'
+                                    className='block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200'
+                                >
+                                    Xác nhận mật khẩu
+                                </label>
+                                <input
+                                    {...register('confirmPassword')}
+                                    className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300'
+                                    type='password'
+                                />
+                            </div>
 
-                    <div className='flex items-center justify-between mt-4'>
-                        <span className='w-1/5 border-b dark:border-gray-600 md:w-1/4'></span>
-                        <Link to='#' className='text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline'>
-                            Sign up with Google
+                            <div className='mt-6'>
+                                <button
+                                    type='submit'
+                                    className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
+                                >
+                                    Đăng ký
+                                </button>
+                            </div>
+                        </form>
+
+                        <div className='flex items-center justify-between mt-4'>
+                            <span className='w-1/5 border-b dark:border-gray-600 md:w-1/4'></span>
+                            <Link
+                                to='#'
+                                className=' text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline'
+                            >
+                                Đăng ký với Google
+                            </Link>
+                            <span className='w-1/5 border-b dark:border-gray-600 md:w-1/4'></span>
+                        </div>
+                        <Link
+                            to='/signin'
+                            className='flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600'
+                        >
+                            <div className='px-4 py-2'>
+                                <svg className='w-6 h-6' viewBox='0 0 40 40'>
+                                    {/* ... (Google logo paths) ... */}
+                                </svg>
+                            </div>
+                            <span className='w-5/6  pr-20 py-3 font-bold text-center '>Đăng nhập</span>
                         </Link>
-                        <span className='w-1/5 border-b dark:border-gray-600 md:w-1/4'></span>
                     </div>
-                    <Link
-                        to='#'
-                        className='flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600'
-                    >
-                        <div className='px-4 py-2'>
-                            <svg className='w-6 h-6' viewBox='0 0 40 40'>
-                                {/* ... (Google logo paths) ... */}
-                            </svg>
-                        </div>
-                        <span className='w-5/6 px-4 py-3 font-bold text-center'>Đăng ký với Google</span>
-                    </Link>
                 </div>
+                <FooterTemplate />
             </div>
-        </div>
+        </>
     )
 }
 

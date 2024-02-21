4125582d64141dce01d650f63bea945ff2d121d2
LuuThiHuyenTrang <cupitk102@gmail.com>
2024-02-22 00:15:50 +0700

    feat: ui + chức năng liên hệ

---
+++ src/hooks/Contact/useContactMutation.ts
@@ -0,0 +1,51 @@
+import { add, remove, update } from '@/services/contact'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useMutation, useQueryClient } from 'react-query'
+
+type useContactMutationProps = {
+    action: 'ADD' | 'UPDATE' | 'DELETE'
+    defaultValues?: any
+    onSuccess?: () => void
+}
+
+export const useContactMutation = ({ action, defaultValues = {}, onSuccess }: useContactMutationProps) => {
+    const queryClient = useQueryClient()
+
+    const { mutate, ...rest } = useMutation({
+        mutationFn: async (contact: any) => {
+            switch (action) {
+                case 'ADD':
+                    return await add(contact)
+                case 'UPDATE':
+                    return await update(contact)
+                case 'DELETE':
+                    return await remove(contact)
+                default:
+                    return null
+            }
+        },
+        onSuccess: () => {
+            onSuccess && onSuccess()
+            queryClient.invalidateQueries({
+                queryKey: ['CATEGORY']
+            })
+        }
+    })
+    const form = useForm({
+        defaultValues
+    })
+    const onSubmit: SubmitHandler<any> = (values) => {
+        console.log(values)
+        mutate(values)
+    }
+    const onRemove = (contact: any) => {
+        mutate(contact)
+    }
+    return {
+        form,
+        onSubmit,
+        onRemove,
+        ...rest
+    }
+}
+//mẫu CATEGORY

75948a057dede70a04dea041b9da4a26a132c564
LuuThiHuyenTrang <cupitk102@gmail.com>
2024-03-01 01:42:29 +0700

    feat: home_tieng viet va - [FE] phân loại  - list product page home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -28,8 +28,9 @@ import List_Bill_Order from '@/pages/Client/AuthPage/List_Bill_Order'
 import Product from '@/pages/Admin/Products'
 import UpdateProfile from '@/pages/Client/AuthPage/UpdateProfile'
 import OrderPage from '@/pages/Client/AuthPage/OrderPage'
-import Sales_at_the_counter from '@/pages/Admin/Sales-at-the-counter'
 import ListContact from '@/pages/Admin/Contact'
+import LayoutUserPage from '@/components/component/LayoutUser'
+import EditVoucher from '@/pages/Admin/ListVoucher/EditVoucher'
 
 const Routers = () => {
     return (
@@ -57,7 +58,6 @@ const Routers = () => {
                 <Route index element={<Dashboard />} />
                 <Route path='dashboard' element={<Dashboard />} />
                 <Route path='products' element={<Product />} />
-                <Route path='sales-at-the-counter' element={<Sales_at_the_counter />} />
                 {/* <Route path='products/:id/edit' element={<EditProduct />}/> */}
                 <Route path='products/add' element={<AddProduct />} />
                 <Route path='categories' element={<ListCategory />} />
